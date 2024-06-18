0853d362d36eda78b9ae1779f94e9647951bfb00
Abusaleh Md Nayeem <abusaleh.nayeem@hana-ltd.com>
2024-06-18 19:57:15 +0600

    update client code

--- api-client/src/app/app.component.ts
+++ api-client/src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GeneratedService } from './generated-api.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ContactFormDto } from './contact-fom-dto';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'api-client';
+
+  contactFormDto: ContactFormDto = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  private readonly restService = Inject(GeneratedService);
+
+  //contactRecipients = toSignal(this.loadRecipient(), { initialValue: [] });
+
+  constructor() {}
+
+  loadRecipient() {
+    this.restService.recipient();
+  }
+
+  onSubmit(frm: NgForm) {
+    console.log(frm.value);
+  }
 }
